2750a99124a9911d15ecb271ac00d142ed322424
Jadyn McClaude <Jadyn.McClaude@msj.edu>
2026-09-13 22:30:48 -0400

    Delete Assignment 1 directory

--- Assignment 1/script1.js
+++
@@ -1,9 +0,0 @@
-console.log("This is javascript page 1");
-
-// Jadyn McClaude - Favorite color: black
-let cookies = 5;
-const people = 2;
-
-cookies = cookies - people;
-
-console.log(`There are currently ${cookies} cookies left`);
